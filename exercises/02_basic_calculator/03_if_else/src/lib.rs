517bcc6e11f3eee3a1765d2553ff2c991420428b
Daniel <daniel_k310a@hotmail.com>
2024-11-04 00:16:11 -0600

    feat: solved 2_ 3 and 4 exercises

--- exercises/02_basic_calculator/03_if_else/src/lib.rs
+++ exercises/02_basic_calculator/03_if_else/src/lib.rs
@@ -1,6 +1,8 @@
 /// Return `true` if `n` is even, `false` otherwise.
 fn is_even(n: u32) -> bool {
-    todo!()
+    let response = if n % 2 == 0 { true } else { false };
+
+    response
 }
 
 #[cfg(test)]
